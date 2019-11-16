dd5d4b2bbb43d07e338adbfcc3da124062d67a55
Ivan <podlipnyak@econ.vsu.ru>
2019-11-16 11:12:00 +0300

    ffmpeg

--- public/test.php
+++ public/test.php
@@ -1,15 +1,10 @@
 <?php
 use Illuminate\Contracts\Console\Kernel;
-use Illuminate\Support\Str;
-use App\User;
-use Illuminate\Support\Facades\Storage;
-use App\MediaFiles;
-use Symfony\Component\HttpFoundation\File\File;
-use App\UploadSessions;
 use Illuminate\Support\Facades\DB;
-use App\Projects;
-use Illuminate\Support\Facades\Auth;
-use App\ProjectInputs;
+use Pbmedia\LaravelFFMpeg\FFMpegFacade as FFMpeg;
+use Illuminate\Container\Container;
+use Pbmedia\LaravelFFMpeg\Media;
+use FFMpeg\FFProbe\DataMapping\Stream;
 require __DIR__ . '/../vendor/autoload.php';
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 $app->make(Kernel::class)->bootstrap();
@@ -17,44 +12,18 @@ $app->make(Kernel::class)->bootstrap();
 /*
  * Test entry point. Should delete this entire file on prod
  */
-
+$container = Container::getInstance();
 DB::enableQueryLog();
 
-// $result = MediaFiles::query()->with('UploadSession')->find(40);
+/* @var $media Media */
+$media = FFmpeg::fromDisk('files')->open('media/1_1.mkv');
+$media->getDurationInSeconds();
 
-// $o = new MediaFiles();
-// $o->name = 'test';
-// $o->user = 1;
-// $o->save();
-
-// $p = new Project();
-// $p->output = $o->id;
-// $p->save();
-
-// $result = Projects::query()->with('inputs')->get();
-
-$project = Projects::query()->with('inputs')->with('output')->find(1);
-// $project = Projects::query()->find(1)->output()->get();
-// $result = $result['inputs']->pluck('id');
-
-/* @var $input MediaFiles */
-// foreach ($project['inputs'] as &$input) {
-//     $input['priority'] = ProjectInputs::query()->where('project', $project['id'])
-//     ->where('media_file', $input['id'])
-//     ->value('priority');
-// }
-
-// $result = $project['inputs']->sortBy('priority')->values()->all();
-$result = $project;
-
-// $result = array_column($result['inputs']->toArray(), 'id');
-// $result = Project::query()->whereHas('output', function($q){
-//     $q->where('user',1);
-// })->get();
-// $result = $media->uploadSession();
+/* @var $stream Stream */
+$stream = $media->getFirstStream();
+$result = $stream->getDimensions()->getHeight();
 
 $log = DB::getQueryLog();
-
 
 response()->json([
     'result' => $result,
